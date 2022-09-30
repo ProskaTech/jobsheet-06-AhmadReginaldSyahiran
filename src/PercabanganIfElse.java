c8ad29baa8f363d45042d68b6d69d538630fcb5b
AhmadReginaldSyahiran <112646010+AhmadReginaldSyahiran@users.noreply.github.com>
2022-09-30 19:51:04 +0700

    Add files via upload

---
+++ src/PercabanganIfElse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/*
+ Created by 21343039_Ahmad Reginald Syahiran
+ */
+
+public class PercabanganIfElse {
+    public static void main(String[] args) {
+        double total_pembelian, diskon = 0;
+
+        Scanner dataMasuk = new Scanner(System.in);
+        System.out.print("Masukkan Total Pembelian : Rp");
+        total_pembelian = dataMasuk.nextDouble();
+
+        if (total_pembelian >= 50000)
+            diskon = 0.1 * total_pembelian;
+        else
+            diskon = 0.02 * total_pembelian;
+
+        System.out.println(" Besarnya diskon : Rp " + diskon);
+
+    }
+}
